6e4b1ff96a543dc7cc9c238fbc7a9aa0ab334eb6
Aron Bergman <bathingrad@gmail.com>
2020-04-28 15:56:59 +0200

    Change BasicTool to become a ToggleToolButton
    The buttons will toggle when clicked and will deselect any already toggled
    buttons in the toolbar.

--- src/LogikUI/Toolbar/ITool.cs
+++ src/LogikUI/Toolbar/ITool.cs
@@ -19,14 +19,39 @@ namespace LogikUI.Toolbar
         public void Draw(CircuitEditor editor, Cairo.Context cr);
     }
 
-    abstract class BasicTool : ToolButton, ITool
+    abstract class BasicTool : ToggleToolButton, ITool
     {
         public CircuitEditor CircuitEditor;
+        private Gtk.Toolbar Toolbar;
 
-        public BasicTool(Image image, string name, CircuitEditor circuitEditor) : base(image, name)
+        public BasicTool(
+            Image image, string name,
+            CircuitEditor circuitEditor,
+            Gtk.Toolbar toolbar
+        ) : base()
         {
+            this.IconWidget = image;
+            this.Label = name;
+            Toolbar = toolbar;
             CircuitEditor = circuitEditor;
             Clicked += BasicTool_Clicked;
+        }
+
+        protected override void OnToggled() {
+            // If the button was activated, we have to make
+            // all other buttons unactive
+            if (this.Active) {
+                for (int i = 0; i < this.Toolbar.NItems; i++) {
+                    ToolItem item = this.Toolbar.GetNthItem(i);
+                    // Make sure that it is a toggle and not a separator
+                    if (item is ToggleToolButton && this != item) {
+                        ToggleToolButton toggle = (ToggleToolButton) item;
+                        if (toggle.Active) {
+                            toggle.Active = false;
+                        }
+                    }
+                }
+            }
         }
 
         private void BasicTool_Clicked(object? sender, EventArgs e)
