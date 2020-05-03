2548eebf3b71f58dbfe4b55be60844c79cbbb8ab
Julius Häger <julius_hager@hotmail.com>
2020-05-03 20:21:34 +0200

    Flushed out the ComponentType enum. Added incomplete Constant component implementation.

---
+++ src/LogikUI/Simulation/Gates/Constant.cs
@@ -0,0 +1,25 @@
+﻿using Cairo;
+using LogikUI.Util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogikUI.Simulation.Gates
+{
+    class Constant : IComponent
+    {
+        public string Name => "Constant";
+        public ComponentType Type => ComponentType.Constant;
+        public int NumberOfPorts => 1;
+
+        public void GetPorts(Span<Vector2i> ports)
+        {
+            ports[0] = new Vector2i(0, 0);
+        }
+
+        public void Draw(Context cr, InstanceData data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
